8e6cb2ace01763fba010b43403f363cea64c5574
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2026-02-25 16:02:04 +0000

    Update assets/js/contrasts/points.js

---
+++ assets/js/contrasts/points.js
@@ -0,0 +1,227 @@
+var Highcharts;
+var optionSelected;
+var dropdown = $('#option_selector');
+var url = '/warehouse/contrasts/menu/menu.json';
+
+
+$.getJSON(url, function (data) {
+
+    $.each(data, function (key, entry) {
+        dropdown.append($('<option></option>').attr('value', entry.catchment_id).text(entry.catchment_name));
+    });
+
+    // Load the first Option by default
+    var defaultOption = dropdown.find("option:first-child").val();
+    optionSelected = dropdown.find("option:first-child").text();
+
+    // Generate
+    generateChart(defaultOption);
+
+});
+
+
+// Dropdown
+dropdown.on('change', function (e) {
+
+    $('#option_selector_title').remove();
+
+    // Save name and value of the selected option
+    optionSelected = this.options[e.target.selectedIndex].text;
+    var valueSelected = this.options[e.target.selectedIndex].value;
+
+    //Draw the Chart
+    generateChart(valueSelected);
+});
+
+
+// Generate graphs
+function generateChart(fileNameKey){
+
+
+    $.getJSON('/warehouse/contrasts/points/' + fileNameKey + '.json', function (source)  {
+
+
+        // Descriptors
+        let fields = source.attributes.columns;
+        let i_station = fields.indexOf('station_name'),
+            i_river = fields.indexOf('river_name'),
+            i_catchment = fields.indexOf('catchment_name');
+
+
+        // split the data set into ...
+        let sectors = [],
+            groupingUnits = [[
+                'hour',                         // unit name
+                [1]                            // allowed multiples
+            ]];
+
+        for (var i = 0; i < source.data.length; i += 1) {
+            
+            sectors.push({
+                name: source.attributes.data[i][i_station],
+                data: source.data[i],
+                description: source.attributes.data[i][i_river],
+                dataGrouping: {
+                    enabled: true,
+                    units: groupingUnits,
+                    dateTimeLabelFormats: {
+                        millisecond: ['%A, %e %b, %H:%M:%S.%L', '-%H:%M:%S.%L'],
+                        second: ['%A, %e %b, %H:%M:%S', '-%H:%M:%S'],
+                        minute: ['%A, %e %b, %H:%M', '-%H:%M'],
+                        hour: ['%A, %e %b, %H:%M', '-%H:%M'],
+                        day: ['%A, %e %b, %Y', '-%A, %e %b, %Y'],
+                        week: ['Week from %A, %e %b, %Y', '-%A, %e %b, %Y'],
+                        month: ['%B %Y', '%B', '-%B %Y'],
+                        year: ['%Y', '%Y', '-%Y']
+                    }
+                },
+                tooltip: {
+                    pointFormat: '<span style="color:{point.color}">\u25CF</span> <b> {point.y:,.3f}% </b> ' +
+                        '<b>|</b> {series.name} <b>|</b> ' + source.attributes.data[i][i_river]
+                },
+                visible: i < 2,
+                showInLegend: true
+            });
+
+        }
+
+
+        // Formatting
+        Highcharts.setOptions({
+            lang: {
+                thousandsSep: ','
+            }
+        });
+
+
+        // Draw a graph
+        Highcharts.stockChart('container0005', {
+
+            rangeSelector: {
+                selected: 0,
+                verticalAlign: 'top',
+                floating: false,
+                inputPosition: {
+                    x: 0,
+                    y: 0
+                },
+                buttonPosition: {
+                    x: 0,
+                    y: 0
+                },
+                inputEnabled: true,
+                inputDateFormat: '%Y-%m-%d'
+            },
+
+            chart: {
+                type: 'spline',
+                zoomType: 'xy',
+                width: 615,
+                marginRight: 265
+            },
+
+            title: {
+                text: '% Change'
+            },
+
+            subtitle: {
+                text: 'Catchment: <b>' + optionSelected + '</b>'
+            },
+
+            credits: {
+                enabled: false
+            },
+
+            legend: {
+                enabled: true,
+                layout: 'vertical',
+                align: 'right',
+                verticalAlign: 'top',
+                maxHeight: 285,
+                width: '60%',
+                useHTML: true,
+                x: 200,
+                itemStyle: {
+                    fontSize: '13px',
+                    fontWeight: 400,
+                    textOverflow: "ellipsis",
+                    marginLeft: -25
+                },
+                navigation: {
+                    activeColor: '#696cff',
+                    animation: true,
+                    arrowSize: 12,
+                    inactiveColor: '#1c1b1b',
+                    style: {
+                        fontWeight: 'bold',
+                        color: '#696cff'
+                    }
+                }
+
+            },
+
+            yAxis: {
+                labels: {
+                    align: 'left',
+                    x: 9
+                },
+                title: {
+                    text: 'percentage change',
+                    x: 0
+                },
+                lineWidth: 2
+            },
+
+            xAxis: {
+                type: 'datetime',
+                crosshair: {
+                    enabled: true
+                }
+            },
+
+            exporting: {
+                buttons: {
+                    contextButton: {
+                        menuItems: [ 'viewFullscreen', 'printChart', 'separator',
+                            'downloadPNG', 'downloadJPEG', 'downloadPDF', 'downloadSVG' , 'separator',
+                            'downloadXLS', 'downloadCSV']
+                    }
+                }
+            },
+
+            tooltip: {
+                shared: true,
+                dateTimeLabelFormats: {
+                    millisecond:"%A, %e %b, %H:%M:%S.%L",
+                    second:"%A, %e %b, %H:%M:%S",
+                    minute:"%A, %e %b, %H:%M",
+                    hour:"%A, %e %b, %H:%M",
+                    day:"%A, %e %B, %Y",
+                    week:"%A, %e %b, %Y",
+                    month:"%B %Y",
+                    year:"%Y"
+                }
+
+            },
+
+            plotOptions: {
+                series: {
+                    pointStart: source['starting'],
+                    pointInterval: source['interval'],
+                    turboThreshold: 4000
+                }
+            },
+
+            series: sectors
+
+        });
+
+
+    }).fail(function() {
+        console.log("Missing");
+        $('#container0005').empty();
+    });
+
+
+
+}
